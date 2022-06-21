c88b0d5fe36eac14980719726dcf4064a6133e50
Kennedy Simiyu <mukhebisimiyu@gmail.com>
2022-06-21 09:23:45 -0700

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
-"""Square Class
 
-A Square Class
-
+"""A class Square that defines a square
+by: (based on 1-square.py)
 """
 
 
 class Square:
 
+    """Square class with a private attribute
+    size.
+    """
+
     def __init__(self, size=0):
-        """__init__
 
-        #The __init__ method initializes the size value of the square.
-
-        #Attributes:
-            size (:obj:`int`, optional): The size of the square.
-
-        #Raises:
-            TypeError: If `size` type is not `int`.
-
-            ValueError: If `size` is less than `0`.
-
+        """Initializes the size variable as a private
+        instance artribute
+        Args:
+            __size (int): The __size of the new square.
         """
 
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
         self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
