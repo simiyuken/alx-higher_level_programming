aa846c73da0db1a1e715aa4eb2a439cc64410a57
Kennedy Simiyu <mukhebisimiyu@gmail.com>
2022-06-28 07:50:49 -0700

    fixing

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-class LockedClass(object):
-    __slots__ = 'first_name'
+"""Defines a locked class."""
+class LockedClass:
+    """
+    Stops the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+    __slots__ = ["first_name"]
