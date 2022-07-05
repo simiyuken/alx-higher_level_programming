e384b0fb875c350bef5491d6213f7089898e6051
Kennedy Simiyu <mukhebisimiyu@gmail.com>
2022-07-05 09:14:11 -0700

    fixing

--- 0x0B-python-input_output/2-pascal_triangle.py
+++ 0x0B-python-input_output/2-pascal_triangle.py
@@ -2,8 +2,8 @@
 
 """Defines a Pascal's Triangle function."""
 
-
 def pascal_triangle(n):
+
     """Represent Pascal's Triangle of size n.
     Returns a list of lists of integers representing the triangle.
     """
